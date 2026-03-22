c1be88fec53a07bd2dbb2fdb6d50f7e253db85c1
Adrian <adrian.kala@icloud.com>
2026-03-22 12:30:19 +0100

    dziala

---
+++ Assets/_Nasze_/tower 1/TowerAttack.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackInterval = 1f;
+    [SerializeField] private int damage = 1;
+
+    private List<EnemyHp> enemiesInRange = new List<EnemyHp>();
+    private Coroutine attackCoroutine;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("widze wejscie");
+        EnemyHp enemy = other.GetComponent<EnemyHp>();
+        if (enemy != null && !enemiesInRange.Contains(enemy))
+        {
+            Debug.Log("enemy to");
+            enemiesInRange.Add(enemy);
+            if (attackCoroutine == null)
+                attackCoroutine = StartCoroutine(AttackCoroutine());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EnemyHp enemy = other.GetComponent<EnemyHp>();
+        if (enemy != null)
+        {
+            enemiesInRange.Remove(enemy);
+        }
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        while (enemiesInRange.Count > 0)
+        {
+            enemiesInRange.RemoveAll(e => e == null);
+            if (enemiesInRange.Count == 0) break;
+
+            EnemyHp target = enemiesInRange[0];
+            target.SubHp(damage);
+            Debug.Log("Tower strzela w enemy!");
+            yield return new WaitForSeconds(attackInterval);
+        }
+        attackCoroutine = null;
+    }
+}
---
+++ Assets/_Nasze_/tower 1/TowerManager.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TowerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private GameObject towerPreviewPrefab;
+    [SerializeField] private Money money;
+    [SerializeField] private int towerCost = 10;
+
+    private bool isBuildMode = false;
+    private GameObject towerPreview;
+
+    void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            EnterBuildMode();
+        }
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            ExitBuildMode();
+        }
+
+        if (isBuildMode && towerPreview != null)
+        {
+            MovePreview();
+        }
+
+        if (isBuildMode && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            PlaceTower();
+        }
+    }
+
+    void EnterBuildMode()
+    {
+        isBuildMode = true;
+
+        towerPreview = Instantiate(towerPreviewPrefab);
+
+        Debug.Log("Tryb budowania ON");
+    }
+
+    void ExitBuildMode()
+    {
+        isBuildMode = false;
+
+        if (towerPreview != null)
+        {
+            Destroy(towerPreview);
+            towerPreview = null;
+        }
+
+        Debug.Log("Tryb budowania OFF");
+    }
+
+    void MovePreview()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        if (groundPlane.Raycast(ray, out float distance))
+        {
+            Vector3 pos = ray.GetPoint(distance);
+
+            Collider towerCollider = towerPrefab.GetComponent<Collider>();
+            float heightOffset = towerCollider != null ? towerCollider.bounds.extents.y : 0f;
+
+            pos.y = heightOffset;
+
+            towerPreview.transform.position = pos;
+        }
+    }
+
+    void PlaceTower()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        if (groundPlane.Raycast(ray, out float distance))
+        {
+            Vector3 pos = ray.GetPoint(distance);
+
+            Collider towerCollider = towerPrefab.GetComponent<Collider>();
+            float heightOffset = towerCollider != null ? towerCollider.bounds.extents.y : 0f;
+
+            pos.y = heightOffset;
+
+            if (money.GetCurrMoney() >= towerCost)
+            {
+                money.SubMoney(towerCost);
+
+                Instantiate(towerPrefab, pos, Quaternion.identity);
+
+                Debug.Log("Wieża postawiona!");
+
+                ExitBuildMode();
+            }
+            else
+            {
+                Debug.Log("Nie masz wystarczająco pieniędzy!");
+            }
+        }
+    }
+}
