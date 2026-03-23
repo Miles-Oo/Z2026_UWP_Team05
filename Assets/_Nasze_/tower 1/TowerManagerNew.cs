41b33e9724a45f82db93c4eb05170a48f4d3950e
NaczoZombi <adrian.kala@icloud.com>
2026-03-23 21:01:10 +0100

    better ui
    zamieniłem kod kładzenia wież, nie wlicza już y, zmieniłem prefaby tower na nowe które są pozbawione zbędnych dla nas rzeczy z assetów.Jest inventory slots który przetrzymuje sloty, każdy slot ma button dla odpowiedniej wieży, text sztywny z ceną wieży oraz camera z assetem wieży, jest to wykorzystane do stworzenia obrazu dla slota aby wiadomo było jaką wieże chcemy kupić

---
+++ Assets/_Nasze_/tower 1/TowerManagerNew.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TowerManagerNew : MonoBehaviour
+{
+    [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private GameObject towerPreviewPrefab;
+    [SerializeField] private Money money;
+    [SerializeField] private int towerCost = 10;
+
+    private bool isBuildMode = false;
+    private GameObject towerPreview;
+
+    void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            EnterBuildMode();
+        }
+
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            ExitBuildMode();
+        }
+
+        if (isBuildMode && towerPreview != null)
+        {
+            MovePreview();
+        }
+
+        if (isBuildMode && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            PlaceTower();
+        }
+    }
+
+    public void EnterBuildMode()
+    {
+        isBuildMode = true;
+        towerPreview = Instantiate(towerPreviewPrefab);
+        Debug.Log("Tryb budowania ON");
+    }
+
+    void ExitBuildMode()
+    {
+        isBuildMode = false;
+
+        if (towerPreview != null)
+        {
+            Destroy(towerPreview);
+            towerPreview = null;
+        }
+
+        Debug.Log("Tryb budowania OFF");
+    }
+
+    void MovePreview()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        if (groundPlane.Raycast(ray, out float distance))
+        {
+            Vector3 pos = ray.GetPoint(distance);
+
+            pos.y = 0f;
+
+            towerPreview.transform.position = pos;
+        }
+    }
+
+    void PlaceTower()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        if (groundPlane.Raycast(ray, out float distance))
+        {
+            Vector3 pos = ray.GetPoint(distance);
+
+            pos.y = 0f;
+
+            if (money.GetCurrMoney() >= towerCost)
+            {
+                money.SubMoney(towerCost);
+
+                Instantiate(towerPrefab, pos, Quaternion.identity);
+
+                Debug.Log("Wieża postawiona!");
+
+                ExitBuildMode();
+            }
+            else
+            {
+                Debug.Log("Nie masz wystarczająco pieniędzy!");
+            }
+        }
+    }
+}
